5d7eedd36ae1787a975e1db6e26983f8bbf0e7c1
Vlad <iwinner521@gmail.com>
2017-02-09 15:24:02 +0200

    Version 2.1 Added authors to the journals

--- BooksAndJournalsApp/BooksAndJournalsApp/Form1.cs
+++ BooksAndJournalsApp/BooksAndJournalsApp/Form1.cs
@@ -23,12 +23,17 @@ namespace BooksAndJournalsApp
                 list.Add(book);
             }
 
-            for (int i = 0; i < 10; i++)
+            for (int journal = 0; journal < 10; journal++)
             {
-                Journal journal = new Journal();
-                journal.Author = "Author" + (i + 1).ToString();
-                journal.Title = "Journal" + (i + 1).ToString();
-                list.Add(journal);
+                Journal tmpjournal = new Journal();
+
+                for (int author = 0; author < 3; author++)
+                {
+                    tmpjournal.Author += "Author" + (author + 1).ToString() + "; ";
+                }
+
+                tmpjournal.Title = "Journal" + (journal + 1).ToString();
+                list.Add(tmpjournal);
             }
 
             InitializeComponent();
