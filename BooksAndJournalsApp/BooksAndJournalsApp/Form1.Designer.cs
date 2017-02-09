9824e7af94bd81329a94283a4bf3992e81900f43
Vlad <iwinner521@gmail.com>
2017-02-09 15:12:43 +0200

    Version 2 WinForms

---
+++ BooksAndJournalsApp/BooksAndJournalsApp/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace BooksAndJournalsApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ListViewer = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.ListViewer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ListViewer
+            // 
+            this.ListViewer.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.ListViewer.Location = new System.Drawing.Point(12, 12);
+            this.ListViewer.Name = "ListViewer";
+            this.ListViewer.Size = new System.Drawing.Size(813, 513);
+            this.ListViewer.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(837, 537);
+            this.Controls.Add(this.ListViewer);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.ListViewer)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView ListViewer;
+    }
+}
+
---
+++ BooksAndJournalsApp/BooksAndJournalsApp/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BooksAndJournalsApp
+{
+    public partial class Form1 : Form
+    {
+        private List<PublishedEdition> list = new List<PublishedEdition>();
+        public Form1()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Book book = new Book();
+                book.Author = "Author" + (i + 1).ToString();
+                book.Title = "Book" + (i + 1).ToString();
+                list.Add(book);
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                Journal journal = new Journal();
+                journal.Author = "Author" + (i + 1).ToString();
+                journal.Title = "Journal" + (i + 1).ToString();
+                list.Add(journal);
+            }
+
+            InitializeComponent();
+            ListViewer.DataSource = list;
+        }
+    }
+}
