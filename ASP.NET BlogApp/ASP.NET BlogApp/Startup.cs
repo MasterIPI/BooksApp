95256e8887abb523778a741fd4674264ce786fba
Vlad <iwinner521@gmail.com>
2017-04-10 14:24:55 +0300

    Version 0 - Empty project

---
+++ ASP.NET BlogApp/ASP.NET BlogApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ASP.NET_BlogApp.Startup))]
+namespace ASP.NET_BlogApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
