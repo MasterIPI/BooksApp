62926bd648f7d13192341a8196bbfb6bc2023f5a
Vlad <iwinner521@gmail.com>
2017-04-17 21:14:34 +0300

    Version 2.0.2 - Removed white spaces from the project

---
+++ ASP.NET_BlogApp/ASP.NET_BlogApp/Models/Comment.cs
@@ -0,0 +1,29 @@
+namespace ASP.NET_BlogApp.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    public partial class Comment
+    {
+        public int Id { get; set; }
+
+        public int PostId { get; set; }
+
+        public DateTime Date { get; set; }
+
+        [Required]
+        [StringLength(128)]
+        public string Name { get; set; }
+
+        [StringLength(128)]
+        public string Email { get; set; }
+
+        [Required]
+        public string Body { get; set; }
+
+        public virtual Post Post { get; set; }
+    }
+}
